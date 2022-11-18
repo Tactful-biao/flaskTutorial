f11a93e27496a617f3117d9debbdc1923c71b93f
BIAO <1208339113@qq.com>
2022-11-18 15:37:27 +0800

    对flask的路由和flask的命令以及flask的运作机制简单了解

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # flaskTutorial
-Learn Flask
-## 记录学习笔记
+```python
+# 路由指定默认值
+@app.route('/greet', defaults={'name': 'Programmer'})
+@app.route('/greet/<name>')
+def greet(name):
+  return f'Hello, {name}'
+```
+
+## flask命令
+
+```python
+# 通过flask hello执行命令, 也可以在command中自定义命令名
+@app.cli.command()
+def hello():
+  click.echo('Hello, Human!')
+```
+
--- hello/app.py
+++ hello/app.py
@@ -1,11 +1,23 @@
+import click
 from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/hi/')
 def hello_world():  # put application's code here
     return '<h1>Hello World!</h1>'
+
+
+@app.route('/greet/', defaults={'name': 'Programmer'})
+@app.route('/greet/<name>')
+def get_name(name):
+    return f'hello, {name}'
+
+@app.cli.command()
+def hello():
+    click.echo('Hello, Human!')
 
 
 if __name__ == '__main__':
